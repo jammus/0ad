07961688421396db8d2509d70e7c229741b7e832
jammus <jammus@gmail.com>
2013-09-10 22:07:40 +0100

    Code review: Remove underscores in property names

--- binaries/data/mods/public/simulation/helpers/WeightedList.js
+++ binaries/data/mods/public/simulation/helpers/WeightedList.js
@@ -1,42 +1,42 @@
 var WeightedList = function()
 {
-    this._elements = [ ];
-    this._totalWeight = 0;
+    this.elements = [ ];
+    this.totalWeight = 0;
 };
 
 WeightedList.prototype.length = function()
 {
-	return this._elements.length;
+	return this.elements.length;
 };
 
 WeightedList.prototype.add = function(item, weight)
 {
 	if (weight === undefined)
 		weight = 1;
-	this._totalWeight += weight;
-	this._elements.push({ "item": item, "weight": weight });
+	this.totalWeight += weight;
+	this.elements.push({ "item": item, "weight": weight });
 };
 
 WeightedList.prototype.removeAt = function(index)
 {
-	var element = this._elements.splice(index, 1);
+	var element = this.elements.splice(index, 1);
 	if (element.length)
-		this._totalWeight -= element[0].weight;
+		this.totalWeight -= element[0].weight;
 };
 
 WeightedList.prototype.itemAt = function(index)
 {
-	var element = this._elements[index];
+	var element = this.elements[index];
 	return element ? element.item : null;
 };
 
 WeightedList.prototype.randomIndex = function() {
 	var element,
-	targetWeight = Math.random() * this._totalWeight,
+	targetWeight = Math.random() * this.totalWeight,
 	cumulativeWeight = 0;
-	for (var index = 0; index < this._elements.length; index++)
+	for (var index = 0; index < this.elements.length; index++)
 	{
-		element = this._elements[index];
+		element = this.elements[index];
 		cumulativeWeight += element.weight;
 		if (cumulativeWeight >= targetWeight)
 			return index;
